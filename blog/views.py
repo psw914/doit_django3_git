2662a3ce9b200d8efc9e39b86d592c5c79183e0c
unknown <psw914@naver.com>
2023-04-05 12:53:23 +0900

    FBV 블로그 상세페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,15 @@ def index(request):
           "posts":posts,
         },
         )
+
+def single_post_page(request,pk):
+    post = models.Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        "blog/single_post_page.html",
+        {
+          "post":post,
+        },
+        
+        )
