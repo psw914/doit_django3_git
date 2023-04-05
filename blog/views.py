aac1cb2dcada9a436f5522214c867481e346d4b8
unknown <psw914@naver.com>
2023-04-05 14:53:31 +0900

    CBV변환... 목록,상세 페이지

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,39 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+
+
 from blog import models
 # Create your views here.
-def index(request):
-    posts = models.Post.objects.all().order_by("-pk")
-    return render(
-        request,
-        "blog/index.html",
-        {
-          "posts":posts,
-        },
-        )
+class PostList(ListView):
+    model = models.Post
+    template_name = "blog/index.html"
+    ordering = "-pk"
 
-def single_post_page(request,pk):
-    post = models.Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        "blog/single_post_page.html",
-        {
-          "post":post,
-        },
+
+# def index(request):
+#     posts = models.Post.objects.all().order_by("-pk")
+#     return render(
+#         request,
+#         "blog/index.html",
+#         {
+#           "posts":posts,
+#         },
+#         )
+
+class PostDetail(DetailView):
+    model = models.Post
+
+
+# def single_post_page(request,pk):
+#     post = models.Post.objects.get(pk=pk)
+
+#     return render(
+#         request,
+#         "blog/single_post_page.html",
+#         {
+#           "post":post,
+#         },
         
-        )
+#         )
