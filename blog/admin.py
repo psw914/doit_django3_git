87eaa41544d76db64f437dfb29852f15a42a682f
unknown <psw914@naver.com>
2023-04-06 17:48:48 +0900

    플러스쉘 설치

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,8 @@ from blog import models
 
 # Register your models here.
 admin.site.register(models.Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(models.Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ import os
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -17,6 +28,9 @@ class Post(models.Model):
 
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+
     def __str__(self):
         return f"[{self.pk}]{self.title} :: {self.author}"
     
@@ -29,3 +43,4 @@ class Post(models.Model):
     
     def get_file_ext(self):
         return self.get_file_name().split(".")[-1]
+
