2f076b02084e396cecdd5badbdd6f998a82ea3b2
unknown <psw914@naver.com>
2023-04-07 16:39:59 +0900

    작성 페이지 권한 설정하기

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import render, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 from blog import models
@@ -107,13 +107,16 @@ def tag_page(request,slug):
         },
     )
 
-class PostCreate(LoginRequiredMixin,CreateView):
+class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
     model = models.Post
     fields = ["title","hook_text","content","head_image","file_upload","category"]
 
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
     def form_valid(self,form):
         current_user = self.request.user
-        if current_user.is_authenticated:
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
             return super(PostCreate,self).form_valid(form)
         else:
