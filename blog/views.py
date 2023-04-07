2a717afd84b6192ab268eed69a646ae5c27c822c
unknown <psw914@naver.com>
2023-04-07 16:29:54 +0900

    로그인 사용자에게만 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 from blog import models
@@ -105,3 +106,15 @@ def tag_page(request,slug):
             "no_cgy":models.Post.objects.filter(category=None).count(),
         },
     )
+
+class PostCreate(LoginRequiredMixin,CreateView):
+    model = models.Post
+    fields = ["title","hook_text","content","head_image","file_upload","category"]
+
+    def form_valid(self,form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate,self).form_valid(form)
+        else:
+            return redirect("/blog")
